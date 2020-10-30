0d63d141fb148b500b7ef7d2fa610f739dff8851
yakobel <yarokobel@gmail.com>
2020-10-30 21:08:47 +0200

    create admin

--- tabl/models.py
+++ tabl/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Tabl(models.Model):
+    title = models.CharField(max_lenght=150)
+    description = models.CharField(max_lenght=300, blank=True)
+    date = models.DateTimeField(auto_now_add=True)
+    done = models.BooleanField(default=False)
+
+
+    def __str__(self):
+        return self.title

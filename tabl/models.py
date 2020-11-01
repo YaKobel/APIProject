e8b8b29802a30235fc9c2c7d4362f3920149c021
yakobel <yarokobel@gmail.com>
2020-11-01 13:55:09 +0200

    edit, fix to api framework

--- tabl/models.py
+++ tabl/models.py
@@ -6,7 +6,7 @@ class Tabl(models.Model):
     title = models.CharField(max_length=150)
     description = models.CharField(max_length=300, blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    done = models.BooleanField(default=False)
+    done = models.BooleanField(default=False, null=False)
 
     def __str__(self):
         return self.title
